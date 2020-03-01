b6aff81e4eb55bfa874318b5d611d683c9e1d465
Wojciech Brożonowicz <wbrozonowicz@o2.pl>
2020-03-01 21:08:46 +0000

    first changes of the main app file

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ const port = 4000
 // SERVER 
 var server = app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 app.get('/', function (req, res) {
-    res.send('hello hi');
+    let textOut = "Witaj szanowny Panie w mojej aplikacji..."
+    res.send(textOut);
 })
